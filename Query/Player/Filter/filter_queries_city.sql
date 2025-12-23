8fcc407e54bdb0437ee54b835fb36ead915f1235
Qiyan Deng <qiyandeng@bit.edu.cn>
2025-12-23 19:22:35 +0800

    update

--- Query/Player/Filter/filter_queries_city.sql
+++ Query/Player/Filter/filter_queries_city.sql
@@ -1,18 +1,18 @@
--- Query 1: 1 (Med_city)
-SELECT state_name, area, city_name FROM Med_city WHERE area = 375.78;
+-- Query 1: 1 (city)
+SELECT state_name, area, city_name FROM city WHERE area = 375.78;
 
--- Query 2: 2 (Med_city)
-SELECT state_name, area, city_name FROM Med_city WHERE area <= 1314.80 AND population > '887642';
+-- Query 2: 2 (city)
+SELECT state_name, area, city_name FROM city WHERE area <= 1314.80 AND population > '887642';
 
--- Query 3: 3 (Med_city)
-SELECT state_name, city_name, population FROM Med_city WHERE state_name != 'Indiana' OR population >= '372,624';
+-- Query 3: 3 (city)
+SELECT state_name, city_name, population FROM city WHERE state_name != 'Indiana' OR population >= '372,624';
 
--- Query 4: 4 (Med_city)
-SELECT gdp, area, state_name FROM Med_city WHERE area >= 976.15 AND state_name != 'Oregon' AND area = 375.78 AND population = '808988';
+-- Query 4: 4 (city)
+SELECT gdp, area, state_name FROM city WHERE area >= 976.15 AND state_name != 'Oregon' AND area = 375.78 AND population = '808988';
 
--- Query 5: 5 (Med_city)
-SELECT gdp, state_name, population FROM Med_city WHERE state_name != 'Ontario' OR city_name = 'Minneapolis' OR gdp != '689000' OR gdp != '344.9';
+-- Query 5: 5 (city)
+SELECT gdp, state_name, population FROM city WHERE state_name != 'Ontario' OR city_name = 'Minneapolis' OR gdp != '689000' OR gdp != '344.9';
 
--- Query 6: 6 (Med_city)
-SELECT population, state_name, city_name FROM Med_city WHERE (state_name = 'Minnesota' AND population = '887642') OR (area >= 976.15 AND area >= 976.15);
+-- Query 6: 6 (city)
+SELECT population, state_name, city_name FROM city WHERE (state_name = 'Minnesota' AND population = '887642') OR (area >= 976.15 AND area >= 976.15);
 
--- Query/Player/Filter/filter_queries_team.sql
+++ Query/Player/Filter/filter_queries_team.sql
@@ -1,36 +1,36 @@
--- Query 1: 1 (Med_team)
-SELECT founded_year, ownership, team_name FROM Med_team WHERE ownership != 'William Chisholm';
+-- Query 1: 1 (team)
+SELECT founded_year, ownership, team_name FROM team WHERE ownership != 'William Chisholm';
 
--- Query 2: 1 (Med_team)
-SELECT ownership, championship, founded_year FROM Med_team WHERE founded_year < 1989;
+-- Query 2: 1 (team)
+SELECT ownership, championship, founded_year FROM team WHERE founded_year < 1989;
 
--- Query 3: 2 (Med_team)
-SELECT founded_year, ownership, team_name FROM Med_team WHERE ownership != 'Jim Morris' AND founded_year >= 1967;
+-- Query 3: 2 (team)
+SELECT founded_year, ownership, team_name FROM team WHERE ownership != 'Jim Morris' AND founded_year >= 1967;
 
--- Query 4: 2 (Med_team)
-SELECT championship, founded_year, location FROM Med_team WHERE location = 'Brooklyn' AND location = 'Memphis';
+-- Query 4: 2 (team)
+SELECT championship, founded_year, location FROM team WHERE location = 'Brooklyn' AND location = 'Memphis';
 
--- Query 5: 3 (Med_team)
-SELECT founded_year, team_name, location FROM Med_team WHERE founded_year < 1949 OR location != 'Oklahoma City';
+-- Query 5: 3 (team)
+SELECT founded_year, team_name, location FROM team WHERE founded_year < 1949 OR location != 'Oklahoma City';
 
--- Query 6: 3 (Med_team)
-SELECT ownership, founded_year, championship FROM Med_team WHERE ownership != 'Herb Simon' OR championship = 3;
+-- Query 6: 3 (team)
+SELECT ownership, founded_year, championship FROM team WHERE ownership != 'Herb Simon' OR championship = 3;
 
--- Query 7: 4 (Med_team)
-SELECT championship, ownership, founded_year FROM Med_team WHERE ownership = 'Paul Allen' AND championship >= 1 AND ownership = 'Professional Basketball Club LLC' AND location = 'Houston';
+-- Query 7: 4 (team)
+SELECT championship, ownership, founded_year FROM team WHERE ownership = 'Paul Allen' AND championship >= 1 AND ownership = 'Professional Basketball Club LLC' AND location = 'Houston';
 
--- Query 8: 4 (Med_team)
-SELECT team_name, championship, location FROM Med_team WHERE championship > 6 AND championship >= 1 AND founded_year = 1989 AND championship <= 1;
+-- Query 8: 4 (team)
+SELECT team_name, championship, location FROM team WHERE championship > 6 AND championship >= 1 AND founded_year = 1989 AND championship <= 1;
 
--- Query 9: 5 (Med_team)
-SELECT championship, founded_year, location FROM Med_team WHERE founded_year <= 1978 OR team_name = 'Dallas Mavericks' OR championship > 3 OR location = 'Atlanta';
+-- Query 9: 5 (team)
+SELECT championship, founded_year, location FROM team WHERE founded_year <= 1978 OR team_name = 'Dallas Mavericks' OR championship > 3 OR location = 'Atlanta';
 
--- Query 10: 5 (Med_team)
-SELECT team_name, founded_year, ownership FROM Med_team WHERE founded_year > 1967 OR team_name != 'Charlotte Hornets' OR team_name = 'Detroit Pistons' OR founded_year >= 1989;
+-- Query 10: 5 (team)
+SELECT team_name, founded_year, ownership FROM team WHERE founded_year > 1967 OR team_name != 'Charlotte Hornets' OR team_name = 'Detroit Pistons' OR founded_year >= 1989;
 
--- Query 11: 6 (Med_team)
-SELECT location, founded_year, team_name FROM Med_team WHERE (founded_year >= 1949 AND founded_year = 1949) OR (ownership != 'Ted Leonsis' AND ownership != 'Professional Basketball Club LLC');
+-- Query 11: 6 (team)
+SELECT location, founded_year, team_name FROM team WHERE (founded_year >= 1949 AND founded_year = 1949) OR (ownership != 'Ted Leonsis' AND ownership != 'Professional Basketball Club LLC');
 
--- Query 12: 6 (Med_team)
-SELECT location, founded_year, team_name FROM Med_team WHERE (founded_year > 1989 AND ownership != 'Gabe Plotkin and Rick Schnall') OR (ownership = 'Atlanta Spirit LLC' AND founded_year = 1967);
+-- Query 12: 6 (team)
+SELECT location, founded_year, team_name FROM team WHERE (founded_year > 1989 AND ownership != 'Gabe Plotkin and Rick Schnall') OR (ownership = 'Atlanta Spirit LLC' AND founded_year = 1967);
 
